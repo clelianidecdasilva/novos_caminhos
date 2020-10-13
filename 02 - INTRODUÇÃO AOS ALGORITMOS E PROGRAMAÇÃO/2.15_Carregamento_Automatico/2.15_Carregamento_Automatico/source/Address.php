fbd2ae259563a6c0e8423e6c7d1fa8b6a6e64369
Cleliani de Cassia da Silva <clelianinutricao@gmail.com>
2020-10-13 00:16:59 -0300

    Atualizando repositório

---
+++ 02 - INTRODUÇÃO AOS ALGORITMOS E PROGRAMAÇÃO/2.15_Carregamento_Automatico/2.15_Carregamento_Automatico/source/Address.php
@@ -0,0 +1,8 @@
+<?php 
+
+namespace source;
+
+class Address
+{
+    
+}
--- www/2.15_Carregamento_Automatico/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-require __DIR__ . '/../framework/config.php';
-PHPClassName('2.15 - Carregamento Automático de Classes');
-
-/*
-*
-*/
-PHPClassSession('Autoload com composer', __LINE__);
